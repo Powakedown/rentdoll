74ab63e306e9f074c0f4fc1c9203c03a6857af2b
loutrusmagnus <cyrilroussy@gmail.com>
2017-08-14 15:23:50 +0200

    Add controller Dolls

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'dolls/index'
+
+  get 'dolls/new'
+
+  get 'dolls/create'
+
+  get 'dolls/update'
+
+  get 'dolls/edit'
+
+  get 'dolls/show'
+
+  get 'dolls/destroy'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
