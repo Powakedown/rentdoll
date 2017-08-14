337405b414bcc60ebb02076e7d6f175f3ce42219
loutrusmagnus <cyrilroussy@gmail.com>
2017-08-14 16:22:26 +0200

    AddControllerDolls routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'dolls/index'
-
-  get 'dolls/new'
-
-  get 'dolls/create'
-
-  get 'dolls/update'
-
-  get 'dolls/edit'
-
-  get 'dolls/show'
-
-  get 'dolls/destroy'
+  resources :dolls
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
