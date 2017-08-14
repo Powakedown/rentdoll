a0b711c93daf4bd1f582e737b74e347c9597ef20
Maxime Boue <abriko@yahoo.fr>
2017-08-14 17:45:30 +0200

    Add Doll#new #create

--- app/controllers/dolls_controller.rb
+++ app/controllers/dolls_controller.rb
@@ -1,11 +1,21 @@
 class DollsController < ApplicationController
+  before_action :doll_params, only: [:create, :update, :edit, :destroy, :show]
+
   def index
   end
 
   def new
+    @doll = Doll.new
   end
 
   def create
+    @doll = Doll.new(doll_params)
+    @user = User.find(params[:id])
+    if @doll.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def update
@@ -18,5 +28,9 @@ class DollsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def doll_params
+    params.require(:doll).permit(:name, :description, :photo, :price)
   end
 end
