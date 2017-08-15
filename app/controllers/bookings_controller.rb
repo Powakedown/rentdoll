42d83b72461c1e9d268e789be7d0cf8de9c607b2
Maxime Boue <abriko@yahoo.fr>
2017-08-15 11:35:52 +0200

    Done booking a doll

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,21 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @doll = @doll = Doll.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user_id = current_user.id
+    @doll = Doll.find(params[:doll_id])
+    @booking.doll_id = params[:doll_id]
+    if @booking.save
+      render 'dolls/show'
+      flash[:notice] = "Vous avez réservé avec succés #{@doll.name} pour le #{@booking.book_at}."
+    else
+      render 'dolls/show'
+    end
   end
 
   def edit
@@ -12,5 +25,11 @@ class BookingsController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:book_at)
   end
 end
