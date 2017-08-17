c21febeef7c9493fa0a071a510127fb707a27082
loutrusmagnus <cyrilroussy@gmail.com>
2017-08-17 18:15:04 +0200

    modif table et page ajouter une doll

--- app/models/doll.rb
+++ app/models/doll.rb
@@ -1,4 +1,8 @@
 class Doll < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  validates :name, presence: true
+  validates :description, presence:true
+  validates :price, presence: true
+  validates :photo, presence: true
 end
--- db/migrate/20170814125400_create_dolls.rb
+++ db/migrate/20170814125400_create_dolls.rb
@@ -2,8 +2,8 @@ class CreateDolls < ActiveRecord::Migration[5.1]
   def change
     create_table :dolls do |t|
       t.string :name
-      t.string :description
-      t.string :price
+      t.text :description
+      t.integer :price
       t.references :user, foreign_key: true
 
       t.timestamps
